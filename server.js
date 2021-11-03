61834028c046e1867b5f00dc55b73a46f0f806d6
Kai Mareving <karska.joanna@gmail.com>
2021-11-03 21:24:44 +0100

    27.5 v.0.1 corrections after mentor talk

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const db = require('./db');
 
 const app = express();
 
-app.use(cors());
+app.use(cors()); /* cross site domain request */
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -14,33 +14,41 @@ app.get('/testimonials', (req, res) => {
   res.json(db.testimonials);
 });
 
-app.get('/testimonials/:id', (req, res) => {
-  res.json(db.testimonials[req.params.id - 1]);
-});
-
 app.get('/testimonials/random', (req, res) => {
   const randID = Math.floor(Math.random() * db.testimonials.length);
   res.json(db.testimonials[randID]);
 });
 
+//! dynamic routing needs to be defined after static routes
+app.get('/testimonials/:id', (req, res) => {
+  res.json(db.testimonials[req.params.id - 1]);
+});
+
+
 app.post('/testimonials', (req, res) => {
-  // const { author, text } = req.body;
-  // const nextId = db.testimonials.length + 1;
-  // res.json(db.testimonials.push({id: nextId, author: author, text: text }));
+  const { author, text } = req.body;
+  const nextId = db.testimonials.length + 1;
+  db.testimonials.push({id: nextId, author: author, text: text });
+  // res.json(db.testimonials);
   res.json({ message: 'OK' });
 });
 
 app.put('/testimonials/:id', (req, res) => {
-  // const { author, text } = req.body;
-  // const edited = db.testimonials[req.params.id - 1];
-  // edited.author = author;
-  // edited.text = text;
+  const { author, text } = req.body;
+  const edited = db.testimonials[req.params.id - 1];
+  if(author){
+    edited.author = author;
+  }
+  if(text){
+    edited.text = text;
+  }
   // res.json(db.testimonials);
   res.json({ message: 'OK' });
 });
 
 app.delete('/testimonials/:id', (req, res) => {
-  // res.json(db.splice((req.params.id - 1), 1));
+  db.testimonials = db.testimonials.filter((testimonial) => testimonial.id != req.params.id);
+  //or db.testimonials.splice((req.params.id - 1), 1);
   res.json({ message: 'OK' });
 });
 
